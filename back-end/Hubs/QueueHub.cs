615da6fcdf8843ce5c6edf325ccedb706747ffc7
Wilfredo Domanico <wilfredo.domanico.jr@gmail.com>
2026-07-05 12:55:58 +0800

    docs: note why QueueHub has no hub methods

--- back-end/Hubs/QueueHub.cs
+++ back-end/Hubs/QueueHub.cs
@@ -4,5 +4,8 @@ namespace back_end.Hubs
 {
     public class QueueHub : Hub
     {
+        // I purposely made it empty and no methods. The reason is that it's for push-only hub.
+        // Client never all it and they only listed to it, which i mapped at app.MapHub<QueHub>("/hubs/queue") in Program.cs
+
     }
 }
